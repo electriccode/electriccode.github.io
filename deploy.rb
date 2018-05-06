e8719eb69db8a35d0917c33c5d94324a1296d42b
Pankaj Phartiyal <pankajphartiyal90@gmail.com>
2018-05-06 09:59:26 +0530

    Annotations in deploy script

--- deploy.rb
+++ deploy.rb
@@ -4,13 +4,17 @@ require 'date'
 cloneurl = "git@heroku.com:phartiyal-stage.git"
 deploymessage = "New deployment @ #{DateTime.now.rfc822.to_s}"
 
-ARGV.each do|a|
+ARGV.each_index do|index|
+    a = ARGV[index]
     if a === "--prod"
         cloneurl = "git@heroku.com:phartiyal.git"
     end
+    if (a.start_with? "--message" or a.start_with? "-m")
+        deploymessage = ARGV[index+1].tr('"', '').tr("'", "")
+    end
 end
 
-print "🔥 Deploying to #{cloneurl}\n"
+print "🔥 Deploying to #{cloneurl} 🔥\n"
 FileUtils.rm_rf('_deploy')
 FileUtils::mkdir_p '_deploy'
 
@@ -18,4 +22,4 @@ system "git clone #{cloneurl} _deploy"
 system 'jekyll build --destination _deploy/_site'
 system "cd _deploy && git add . && git commit -am '#{deploymessage}' && git push origin master"
 
-print "✅ Deploying to #{cloneurl} successful!!\n"
+print "✅ Deployed to #{cloneurl} successful!! ✅\n"
